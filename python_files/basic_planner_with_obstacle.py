d2bfb0745769aa9c33426994ee5e309ae2015c59
Devvrat Arya <devvratarya15@gmail.com>
2014-06-05 20:05:41 +0200

    initial planning code with and with out obstacle

---
+++ python_files/basic_planner_with_obstacle.py
@@ -0,0 +1,19 @@
+from openravepy import *
+import time
+env = Environment()
+env.Load('../environment/rm_with_obstacle.env.xml')
+env.SetViewer('qtcoin')
+robot = env.GetRobots()[0]
+current_pos = robot.GetDOFValues()
+init_pos = current_pos
+time.sleep(2)
+current_pos[0]=0.01
+current_pos[1]=0.01
+robot.SetDOFValues(current_pos)
+RaveSetDebugLevel(DebugLevel.Debug)
+goal_pos = [0.01,0.01,0.0,1.15,1.2]
+manipprob = interfaces.BaseManipulation(robot)
+manipprob.MoveManipulator(goal=goal_pos)
+
+robot.WaitForController(0)
+time.sleep(5)
